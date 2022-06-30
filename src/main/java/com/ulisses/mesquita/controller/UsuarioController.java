5b291d88cf70da0203e87a38cc74a746fdef9499
Ulisses Mesquita <ulisses.mesquita@xpon.com.br>
2022-06-29 21:07:51 -0300

    CRUD OK

--- src/main/java/com/ulisses/mesquita/controller/UsuarioController.java
+++ src/main/java/com/ulisses/mesquita/controller/UsuarioController.java
@@ -2,8 +2,13 @@ package com.ulisses.mesquita.controller;
 
 import java.util.List;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +20,8 @@ import com.ulisses.mesquita.repository.UsuarioRepository;
 @RequestMapping("/usuario")
 public class UsuarioController {
 	
-	UsuarioRepository usuarioRepository;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@GetMapping
 	public List<Usuario> listarUsuarios(){
@@ -23,9 +29,22 @@ public class UsuarioController {
 	}
 	
 	@PostMapping
-	public Usuario adcionarUsuario(@RequestBody Usuario usuario){
+	public Usuario adicionarUsuario(@RequestBody Usuario usuario){
 		return usuarioRepository.save(usuario);
 	}
 	
-
+	@DeleteMapping("usuario/delete/{id}")
+	public void deletarUsuario(@PathVariable Long id ) {
+		usuarioRepository.deleteById(id);
+	}
+	
+	@PutMapping("{id}")
+	public Usuario alterarUsuario(@PathVariable Long id, @RequestBody Usuario usuario){
+		Usuario usuarioTeste = usuarioRepository.getReferenceById(id);
+		
+		usuarioTeste.setEmail(usuario.getEmail());
+		usuarioTeste.setPassword(usuario.getPassword());
+		
+		return   usuarioRepository.save(usuarioTeste);
+	}
 }
--- src/main/java/com/ulisses/mesquita/model/Usuario.java
+++ src/main/java/com/ulisses/mesquita/model/Usuario.java
@@ -4,19 +4,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "usuario")
 public class Usuario {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private String usuario;
+	private String email;
 	private String password;
+	
+	
+	
+	
+	
+	
 	
 	
 	
