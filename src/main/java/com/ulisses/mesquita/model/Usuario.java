fef356bfb0335bc90bda3cb73ca080b36eec1866
Ulisses Mesquita <ulisses.mesquita@xpon.com.br>
2022-07-04 19:58:19 -0300

    Finalizar com tratativas e HttpStatus

--- src/main/java/com/ulisses/mesquita/model/Usuario.java
+++ src/main/java/com/ulisses/mesquita/model/Usuario.java
@@ -1,10 +1,14 @@
 package com.ulisses.mesquita.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -17,7 +21,16 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotEmpty
+	@NotBlank(message = "The email cannot be a blank field")
+	@NotNull(message = "The email cannot be empty")
+	@Column(nullable = false)
 	private String email;
+	
+	@NotEmpty
+	@NotBlank(message = "The password cannot be a blank field")
+	@NotNull(message = "The password cannot be empty")
+	@Column(nullable = false)
 	private String password;
 	
 	
--- src/main/java/com/ulisses/mesquita/services/UsuarioService.java
+++ src/main/java/com/ulisses/mesquita/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.ulisses.mesquita.services;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
